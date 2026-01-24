a6981379d012f216d89ec8572e47ccd6ed579cf8
chae1125 <y72682685@gmail.com>
2026-01-25 02:58:07 +0900

    fix: 검색 입력 컴포넌트 스타일 수정 및 버튼 위치 조정

--- src/components/input/SearchInput.tsx
+++ src/components/input/SearchInput.tsx
@@ -14,7 +14,7 @@ export function SearchInput({
   placeholder = '원하는 케이크샵을 검색해 보세요!',
 }: SearchInputProps) {
   return (
-    <div className="relative w-[90%] max-w-md mx-auto">
+    <div className="relative w-full max-w-lg mx-auto">
       <input
         type="text"
         value={value}
@@ -23,13 +23,14 @@ export function SearchInput({
         onKeyDown={(e) => {
           if (e.key === 'Enter') onSubmit?.()
         }}
-        className="h-12 w-full rounded-[14px] border border-input-border px-5 pr text-md text-black outline-none bg-input-bg focus:border-main-pink-200"
+        className="h-12 w-full rounded-[14px] border-[var(--color-sub-gray-50)] border pl-12 pr-5 text-md text-black outline-none bg-white focus:border-main-pink-200"
       />
 
       <button
         type="button"
         onClick={onSubmit}
-        className="absolute right-4 top-1/2 -translate-y-1/2 text-sub-gray-100"
+        aria-label="검색"
+        className="absolute left-4 top-1/2 -translate-y-1/2 text-[var(--color-sub-gray-100)]"
       >
         <Search size={20} />
       </button>
