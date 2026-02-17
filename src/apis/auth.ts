c23463b67e6176cc2672c0847c67e78748f0223e
chae1125 <y72682685@gmail.com>
2026-02-17 20:20:44 +0900

    refactor: auth.ts에서 refreshToken 함수 제거 및 axios.ts로 이동

--- src/apis/auth.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export const refreshToken = async () => {
-  return axios.post('/api/users/refresh', {}, { withCredentials: true })
-}
-
-export default { refreshToken }
